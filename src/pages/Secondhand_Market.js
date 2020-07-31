378abc378422840b9b2b2f3874b5b0c593b62d15
e <1426903325@qq.com>
2020-07-31 13:51:01 +0800

    增加mobx

--- src/pages/Secondhand_Market.js
+++ src/pages/Secondhand_Market.js
@@ -1,13 +1,11 @@
-import React, { Component } from 'react'
+﻿import React, { Component } from 'react'
 // 二手市场
 export default class Secondhand_Market extends Component {
     render() {
         return (
             <div>
                 {/* 二手馆 */}
-              <div>
-                  <img src="" alt="">
-              </div>
+              
             </div>
         )
     }
