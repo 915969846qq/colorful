9528e50de42a9ab8fbbf3dca3dd69356f1bd8089
chenming <chenming159289@gmail.com>
2020-07-31 18:07:44 +0800

    商品组件

---
+++ src/component/Secondhand_market/secprodmarket-message.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+class secprodmarket_detail extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            sec_detailname:"处理办公桌椅",
+            sec_detailtitle:""
+         }
+    }
+    render() { 
+        return ( 
+            <div>
+                {/* 详细信息 */}
+               <div>
+                   <p></p>
+               </div>
+               {/* 卖家头像 */}
+               <div></div>
+               {/* 商品描述与图片 */}
+               <div></div>
+            </div>
+         );
+    }
+}
+ 
+export default secprodmarket_detail;
