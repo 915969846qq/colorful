b584da5ac8328fcc0bcf64f62185445ea02c382b
e <1426903325@qq.com>
2020-07-31 16:33:07 +0800

    添加工匠页面

--- src/pages/Secondhand_Market.js
+++ src/pages/Secondhand_Market.js
@@ -1,7 +1,7 @@
 ﻿import React, { Component } from 'react'
 // 二手市场
 export default class Secondhand_Market extends Component {
-<<<<<<< HEAD
+
     render() {
         return (
             <div>
@@ -10,9 +10,5 @@ export default class Secondhand_Market extends Component {
             </div>
         )
     }
-=======
-  render() {
-    return <div>{/* 二手馆 */}</div>
-  }
->>>>>>> 6c13409a1ede14ad527311276454d4d5429cdb63
+
 }
