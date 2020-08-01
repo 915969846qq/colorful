cf542837cf060ca5822f39c2643b18ef706d07b3
Spiritualdream <2495944984@qq.com>
2020-08-01 09:57:54 +0800

    修改

--- src/pages/Secondhand_Market.js
+++ src/pages/Secondhand_Market.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react'
 import Secdetail from '../component/Secondhand_market/secprodmarket-message'
 // 二手市场
 export default class Secondhand_Market extends Component {
-    render() {
-        return (
-            <div>
-                {/* 二手馆 */}
-             <Secdetail></Secdetail>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        {/* 二手馆 */}
+        <Secdetail></Secdetail>
+      </div>
+    )
+  }
 }
