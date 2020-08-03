a5ac8f10a05ba9e8cae5a7aecfb9e54ad9ebbc57
1430617595 <1430617595@qq.com>
2020-08-03 16:31:14 +0800

    1

--- src/component/Register.js/Sign_Register.js
+++ src/component/Register.js/Sign_Register.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+
+
+
 //注册
 export default class Sign_Register extends Component {
     render() {
         return (
             <div>
-                
+                注册
             </div>
         )
     }
