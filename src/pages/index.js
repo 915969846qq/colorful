f48910f7c34d02befef7a9b24a63f968dd4db170
915969846qq <915969846@qq.com>
2020-08-03 11:12:28 +0800

    首页上传 2020.8.3   第二版

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react'
-import { NavLink, BrowserRouter as Router,Route } from 'react-router-dom'
+import { NavLink,BrowserRouter as Router,Route } from 'react-router-dom'
 import '../Css/index.css'
 import '../Css/iconfont-index/iconfont.css'
 import { Carousel } from 'antd'
-
+import Building_materials_Museum from './Building_materials_Museum'
+import Craftsman_Pavilion from './Craftsman_Pavilion'
+import Decoration_Museum from './Decoration_Museum'
+import Furniture_Museum from './Furniture_Museum'
+import Home_Museum from './Home_Museum'
+import Secondhand_Market from './Secondhand_Market'
+import Style_pavilion from './Style_pavilion'
+import Personal_Center_index from './Personal_Center_index'
 
 
 //首页
@@ -17,7 +24,14 @@ export default class index extends Component {
         return (
             <div id="body"> 
                     <Router>
-                        
+                        <Route path="/Building_materials_Museum"  component={Building_materials_Museum}></Route>
+                        <Route path="/Craftsman_Pavilion" component={Craftsman_Pavilion}></Route>
+                        <Route path="/Decoration_Museum" component={Decoration_Museum}></Route>
+                        <Route path="/Furniture_Museum" component={Furniture_Museum}></Route>
+                        <Route path="/Home_Museum" component={Home_Museum}></Route>
+                        <Route path="/Personal_Center_index" component={Personal_Center_index}></Route> 
+                        <Route path="/Secondhand_Market" component={Secondhand_Market}></Route>
+                        <Route path="/Style_pavilion" component={Style_pavilion}></Route>     
                         <div className="body">
                         
                         <div id="site-nav" data-spm="a2226mz" role="navigation">
@@ -1217,7 +1231,7 @@ export default class index extends Component {
                         </div>
                        
                     </div>     
-                    </Router>
+                </Router>
                     
                 
                    
